f368b9ed2a9c98006a506495ae285483227ab04b
MayankManjith1 <123382162+MayankManjith1@users.noreply.github.com>
2023-06-20 18:17:04 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+firebaseConfig = {
+    apiKey: "AIzaSyDTOd1Mp4QjdrWWuvzFETg4-uHJ6hXUoEw",
+    authDomain: "kwitter-e8e33.firebaseapp.com",
+    databaseURL: "https://kwitter-e8e33-default-rtdb.firebaseio.com",
+    projectId: "kwitter-e8e33",
+    storageBucket: "kwitter-e8e33.appspot.com",
+    messagingSenderId: "83082335204",
+    appId: "1:83082335204:web:ad0cb9813e71cc04506df1"
+  };
+  firebase.initializeApp(firebaseConfig);
